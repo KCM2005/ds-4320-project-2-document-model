054df2ffdfc7f7259f921b9bb6c2e38ba04dcd08
Kalenga Mumba <157423014+KCM2005@users.noreply.github.com>
2026-04-29 19:54:05 -0400

    Implement data retrieval from MongoDB
    Added function to retrieve dataset from MongoDB and convert it into a pandas DataFrame.

--- mongo_retrieve.py
+++ mongo_retrieve.py
@@ -1,1 +1,29 @@
+from pymongo import MongoClient
+from urllib.parse import quote_plus
+import pandas as pd
+import logging
 
+def load_from_mongo():
+    """
+    Retrieves dataset from MongoDB and converts it into a pandas DataFrame.
+    """
+
+    #Load data from MongoDB into DataFrame
+    try:
+        username = quote_plus(MONGODB_USERNAME)
+        password = quote_plus(MONGODB_PASSWORD)
+
+        uri = f"mongodb+srv://{username}:{password}@diabetics.049ou64.mongodb.net/?appName=Diabetics"
+
+        client = MongoClient(uri)
+
+        db = client["diabetics-db"]
+        collection = db["readmissions"]
+
+        df = pd.DataFrame(list(collection.find()))
+        df = df.reset_index(drop=True)
+
+        logging.info("Data loaded from MongoDB")
+
+    except Exception as e:
+        logging.error(f"Error loading from MongoDB: {e}")
